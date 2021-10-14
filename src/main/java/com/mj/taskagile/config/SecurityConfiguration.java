bd3e3f6a1dfcc9325adb0722939013c564a795d7
mj-win <mj-win@gmail.com>
2021-10-14 21:23:13 +0900

    implement PasswordEncryptorDelegate

--- src/main/java/com/mj/taskagile/config/SecurityConfiguration.java
+++ src/main/java/com/mj/taskagile/config/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.mj.taskagile.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {    
@@ -32,5 +35,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login?logged-out")
                 .and()
             .csrf().disable();
-    }    
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/mj/taskagile/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/com/mj/taskagile/domain/common/security/PasswordEncryptorDelegator.java
@@ -1,12 +1,18 @@
 package com.mj.taskagile.domain.common.security;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Component
 public class PasswordEncryptorDelegator implements PasswordEncryptor {
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public String encrypt(String rawPassword) {
-        return rawPassword;
+        return passwordEncoder.encode(rawPassword);
     }
 }
